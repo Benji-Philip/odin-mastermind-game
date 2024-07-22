735ac8f8f2eb06adcdce029f5f2acbe7aa728017
Benji-Philip <99130709+Benji-Philip@users.noreply.github.com>
2024-07-22 21:38:14 +0530

    plan of the game

---
+++ main.rb
@@ -0,0 +1,11 @@
+# 12 turns
+# 4 colors
+# guess the code
+# numbers are 0 1 2 3 4 5 6 7 8 9
+# computer generates 4 digit code
+# player has 12 chances to guess it right
+# computer checks if any number exists in the code
+# computer checks if the number is in the correct position if it does exist in the code
+# if player guesses within 12 turns they win otherwise they lose
+
+# components - player computer game game-rounds secret-code guesses feedback history-of-player
